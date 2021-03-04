0bbbd7ae24c928abcc7edefed04e73f3368c415d
floriansaubin <saubinflorian@gmail.com>
2021-03-04 17:01:27 +0100

    need adjustement

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -11,9 +11,9 @@ class DosesController < ApplicationController
   def create
     @cocktail = Cocktail.find(params[:cocktail_id])
     @dose = Dose.new(dose_params)
-    @dose.cocktail_id = @cocktail
+    @dose.cocktail = @cocktail
     if @dose.save
-      redirect_to cocktails_path
+      redirect_to cocktail_path(@cocktail)
     else
       render :new
     end
@@ -21,16 +21,14 @@ class DosesController < ApplicationController
 
   def destroy
     @dose = Dose.find(params[:id])
-    @cocktail = Cocktail.find(params[:cocktail_id])
-    @dose.cocktail_id = @cocktail
     @dose.destroy
 
-    redirect_to cocktails_path
+    redirect_to cocktail_path(@dose.cocktail)
   end
 
   private
 
   def dose_params
-    params.require(:dose).permit(:description)
+    params.require(:dose).permit(:description, :ingredient_id)
   end
 end
