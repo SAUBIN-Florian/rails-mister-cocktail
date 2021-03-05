836237c960e20d29250a8a7e2a4b5a219f193f4d
floriansaubin <saubinflorian@gmail.com>
2021-03-05 11:46:10 +0100

    op

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -2,5 +2,5 @@ class Dose < ApplicationRecord
   belongs_to :cocktail
   belongs_to :ingredient
   validates :description, presence: true
-  validates :cocktail, :ingredient, presence: true, uniqueness: { scope: :ingredient }
+  validates :cocktail, :ingredient, presence: true, uniqueness: { scope: :ingredient_id }
 end
